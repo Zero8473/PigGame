02b4a7aeeaa15efecf9951f1e8c591b28663c1d3
Lisa <michllisa.dev@gmail.com>
2023-10-12 14:30:59 +0200

    dice roll and animation implemented

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+//Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1'); //a little faster than querySelector
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+btnRoll.addEventListener('click', function () {
+  //1. Generate random dice
+  const dice = Math.trunc(Math.random() * 6 + 1);
+  console.log(dice);
+
+  //2. display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  //3. check if roll is 1: if true switch player, else add roll to score
+});
