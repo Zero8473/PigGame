8d6391b0d8d32b27f3cd1663b7b269e1bc8b8f1e
Lisa <michllisa.dev@gmail.com>
2023-10-13 11:02:05 +0200

    completed the roll functionality

--- script.js
+++ script.js
@@ -2,22 +2,44 @@
 //Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1'); //a little faster than querySelector
+const current0EL = document.getElementById('current--0');
+const current1EL = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const player0EL = document.querySelector('.player--0');
+const player1EL = document.querySelector('.player--1');
 
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+//Event for clicking the Roll button
 btnRoll.addEventListener('click', function () {
   //1. Generate random dice
   const dice = Math.trunc(Math.random() * 6 + 1);
   console.log(dice);
-
   //2. display dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
+
   //3. check if roll is 1: if true switch player, else add roll to score
+  if (dice !== 1) {
+    //Add rolled number to current score
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    return;
+  }
+
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  //switch player
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0EL.classList.toggle('player--active');
+  player1EL.classList.toggle('player--active');
 });
